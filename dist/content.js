7fb7b6c5cca74a33f823fde2a9cf1811e2af5bb6
Petrushin Anton <petrushin.a@live.ru>
2021-12-21 04:20:21 +0300

    ver. 0.0.1

---
+++ dist/content.js
@@ -0,0 +1,90 @@
+
+
+
+const url_string = window.location.href
+const url = new URL(url_string)
+
+const payoutNavLink = document.querySelector('[href="https://pool.gravitsapa.space/payouts"]')
+const statNavLink = document.querySelector('[href="https://pool.gravitsapa.space/wallet"]') 
+
+let lw = getLocalWallet()
+let tw = url.searchParams.get("tw")
+let currentPath = url.pathname
+
+
+
+console.log("current path", currentPath)
+
+console.log("lw ", lw)
+console.log("tw ", tw)
+
+const hasWallet = new Boolean( lw || tw )
+
+console.log("haswallet" ,hasWallet)
+
+if (tw) {
+    setLocalWallet(tw)
+    switchHref(tw, payoutNavLink, statNavLink)
+
+} else if (getLocalWallet()) {
+    lw = getLocalWallet()
+    switch (currentPath){
+        case "/payouts":
+            loadLocalWallet()
+        break
+        case "/wallet":
+            loadLocalWallet()
+        break
+        case "/founds":
+            foundsChangeHref()
+        break
+    }
+} 
+
+function foundsChangeHref(){
+    var foundsLinks = document.querySelectorAll("td > a")
+    foundsLinks.forEach(u => {
+        var founderWallet = new URL(u.getAttribute("href")).pathname.split("/")[2]
+        u.removeAttribute("target")
+        u.setAttribute("href", "https://pool.gravitsapa.space/wallet?tw="+founderWallet )
+    })
+}
+
+function loadLocalWallet (){
+    if (window.confirm("Перейти в сохраненный кошелек?")) {
+        window.location.href = url_string +"?tw="+ lw;
+    }
+}
+
+function setLocalWallet(w){
+    localStorage.setItem("wal", w)
+
+
+    /*
+    chrome.storage.local.set({wal: w}, function() {
+        console.log('Value is set to ' + tw)
+    }) */
+}
+
+function getLocalWallet() {
+    
+     return localStorage.getItem("wal")
+    
+    
+    
+    /*chrome.storage.local.get(['wal'], function (result) {
+        console.log('Value in local.storage is ' + JSON.stringify(result.wal))
+      })*/
+}
+
+
+function switchHref(wallet, ...args) {
+    console.log(args)
+    args.forEach(l => {
+        var href=l.getAttribute("href") + "?tw=" + wallet
+        l.setAttribute("href", href)
+    });
+}
+
+
+
